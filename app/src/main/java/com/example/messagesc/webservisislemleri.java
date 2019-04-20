4e05507492b3c6f8b787309fe6430ceb86b9c4c7
cagri <cagri_oncel123@hotmail.com>
2019-04-20 04:16:50 +0300

    webservisişlemleri adında bir class oluşturuldu.veritabanı metotları buraya yazılarak kod karmaşası engellendi.

---
+++ app/src/main/java/com/example/messagesc/webservisislemleri.java
@@ -0,0 +1,93 @@
+package com.example.messagesc;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class webservisislemleri {
+    private void yaziyiAl(String url2, final Context context) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mesajC.tv.setText("");
+                baslik = "";
+                alinanVerileriNesneyeDoldur(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Listeleme gerçekleştirilemedi", Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> deger = new HashMap<>();
+                deger.get("eklenecekVeri");
+                return deger;
+            }
+        };
+        MySingleton.getInstance(context).addToRequest(stringRequest);
+
+    }
+
+    public void yaziyiGonder(String url, final Context context) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String url2 = "http://sadakatsizcpre.tr.ht/getir.php";
+                Toast.makeText(context, "" + mesajC.ad.getText().toString() + "eklendi", Toast.LENGTH_LONG).show();
+                mesajC.ad.setText("");
+                yaziyiAl(url2,context);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "İşlem gerçekleştirilemedi", Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> deger = new HashMap<>();
+                deger.put("eklenecekVeri", mesajC.ad.getText().toString());
+
+                return deger;
+            }
+        };
+        MySingleton.getInstance(context).addToRequest(stringRequest);
+    }
+
+    String baslik = "";
+
+    private void alinanVerileriNesneyeDoldur(String string) {
+        try {
+            JSONObject response = new JSONObject(string);
+            JSONArray arrYazilar = response.getJSONArray("donenVeriler");
+            for (int i = 0; i < arrYazilar.length(); i++) {
+                JSONObject yazi = arrYazilar.getJSONObject(i);
+                baslik += yazi.getString("tag") + "\n";
+
+            }
+            mesajC.tv.setText(baslik);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
